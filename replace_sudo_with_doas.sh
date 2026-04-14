727f3703e41c55e13d15077e61f5b636c68cc248
Kinderfeld <industrialmachine2000@protonmail.com>
2026-04-14 19:28:10 +0300

    small improvements

--- replace_sudo_with_doas.sh
+++ replace_sudo_with_doas.sh
@@ -27,7 +27,7 @@ sudo_to_ignore()
     printf "[<==] Adding 'sudo' to ignored packages...\n"
 
     mkdir -p /etc/xbps.d
-    if [ ! -e /etc/xbps.d/ignore.conf ] || ! grep -q '^ignorepkg=sudo' /etc/xbps.d/ignore.conf; then
+    if [ ! -e /etc/xbps.d/ignore.conf ] || ! grep -q "^ignorepkg=sudo" /etc/xbps.d/ignore.conf; then
         echo "ignorepkg=sudo" >> /etc/xbps.d/ignore.conf
     fi
 }
@@ -44,7 +44,7 @@ remove_sudo()
         netbsd)  pkgin remove sudo ;;
         openbsd) pkg_delete sudo ;;
         void)    sudo_to_ignore && xbps-remove sudo ;;
-        *)       printf "[!] Unsupported OS: '$1'\n"; exit 1 ;;
+        *)       printf "[!] Unsupported OS: '%s'\n" "$1"; exit 1 ;;
     esac
 }
 
@@ -74,7 +74,7 @@ configure_doas()
         netbsd)  echo "${config}" > /usr/pkg/etc/doas.conf || echo "${config}" > /etc/doas.conf ;;
     esac
 
-    ln -s $(which doas) /usr/bin/sudo
+    ln -s "$(which doas)" /usr/bin/sudo
 }
 
 root_check()
@@ -90,7 +90,7 @@ main()
     printf "[*] Starting 'sudo' replacement...\n"
 
     printf "[==>] Enter your OS family [debian, arch, alpine, freebsd, openbsd, netbsd, void]: "
-    read os
+    read -r os
 
     remove_sudo "${os}"
     install_doas "${os}"
@@ -101,4 +101,3 @@ main()
 
 root_check
 main
-
